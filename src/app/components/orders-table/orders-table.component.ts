3895c39a0acc04a7d022f84fdfb990ddb0ea446a
rizbons <rizbo.ns.bk@gmail.com>
2022-10-11 16:47:02 +0200

    order-panel

--- src/app/components/orders-table/orders-table.component.ts
+++ src/app/components/orders-table/orders-table.component.ts
@@ -58,12 +58,19 @@ export class OrdersTableComponent implements OnInit, OnChanges {
   compare(a: number | string, b: number | string, isAsc: boolean) {
     return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
   }
+  selectOrder(orderId) {
+    alert(orderId);
+  }
   ngOnChanges(changes: SimpleChanges): void {
-    this.orders$.subscribe((res) => (this.orders = res));
-    this.sortedData = this.orders.slice();
+    this.orders$.subscribe((res) => {
+      this.orders = res;
+      this.sortedData = this.orders.slice();
+    });
   }
   ngOnInit(): void {
-    this.orders$.subscribe((res) => (this.orders = res));
-    this.sortedData = this.orders.slice();
+    this.orders$.subscribe((res) => {
+      this.orders = res;
+      this.sortedData = this.orders.slice();
+    });
   }
 }
